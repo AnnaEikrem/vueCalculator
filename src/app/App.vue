1762f7b5d05d53c986a63042c757073498af930b
Anna E <112336610+AnnaEikrem@users.noreply.github.com>
2022-11-05 08:15:04 +0100

    first commit

---
+++ src/app/App.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="container">
+		<Calculator/>
+		<Converter/>
+		<hr class="line red">
+		<hr class="line yellow">
+		<hr class="line blue">
+	</div>
+</template>
+
+<script>
+	import Calculator from '../components/Calculator.vue'
+	import Converter from '../components/Converter.vue'
+
+	export default {
+		components: {
+			Calculator,
+			Converter
+		}
+	}
+</script>
+
+
+<style>
+	* {
+		box-sizing: border-box;
+		padding: 0;
+		margin: 0;
+		font-size: 20px;
+	}
+
+	.container {
+		position: relative;
+		width: 70%;
+		height: 80vh;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		
+		margin: 3rem;
+		border-radius: 0.3rem;
+		border: 0.5rem solid #6f5643;
+	}
+
+	.red {
+		background: rgb(242, 90, 90);
+		position: absolute;
+		width: 40%;
+		height: 0.6rem;
+		bottom: 2.2rem;
+		left: 0;
+		border: none;
+		border-radius: 0 10px 10px 0;
+		z-index: 10;
+	}
+
+	.yellow {
+		background: rgb(219, 201, 48);
+		position: absolute;
+		width: 40%;
+		height: 0.6rem;
+		bottom: 1.3rem;
+		left: 0;
+		border: none;
+		border-radius: 0 10px 10px 0;
+		z-index: 10;
+	}
+
+	.blue {
+		background: rgb(92, 92, 180);
+		position: absolute;
+		width: 40%;
+		height: 0.6rem;
+		bottom: 0.5rem;
+		left: 0;
+		border: none;
+		border-radius: 0 10px 10px 0;
+		z-index: 10;
+	}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,229 @@
+<template>
+	<section class="calculator-main">
+		<h1 class="calculator-main__headline">Calculator</h1>
+
+		<div class="calculator-main__output">
+			{{ showingResult ? currentResult : currentInput}}
+		</div>
+
+		<div class="calculator-main__digits">
+			<button @click="clearResult" class="calculator-main__button calculator-main__clear">C</button>
+
+			<button @click="handleOperatorInput('/')" class="calculator-main__button calculator-main__operator">/</button>
+
+			<button @click="handleDigitInput(7)" class="calculator-main__button">7</button>
+
+			<button @click="handleDigitInput(8)" class="calculator-main__button">8</button>
+
+			<button @click="handleDigitInput(9)" class="calculator-main__button">9</button>
+
+			<button @click="handleOperatorInput('*')" class="calculator-main__button calculator-main__operator">*</button>
+
+			<button @click="handleDigitInput(4)" class="calculator-main__button">4</button>
+
+			<button @click="handleDigitInput(5)" class="calculator-main__button">5</button>
+
+			<button @click="handleDigitInput(6)" class="calculator-main__button">6</button>
+
+			<button @click="handleOperatorInput('-')" class="calculator-main__button calculator-main__operator">-</button>
+
+			<button @click="handleDigitInput(1)" class="calculator-main__button">1</button>
+
+			<button @click="handleDigitInput(2)" class="calculator-main__button">2</button>
+
+			<button @click="handleDigitInput(3)" class="calculator-main__button">3</button>
+
+			<button @click="handleOperatorInput('+')" class="calculator-main__button calculator-main__operator">+</button>
+
+			<button @click="handleDigitInput(0)" class="calculator-main__button calculator-main__zero">0</button>
+
+			<button @click="handleDigitInput('.')" class="calculator-main__button">,</button>
+
+			<button @click="handleEqualsInput" class="calculator-main__button calculator-main__operator">=</button> 
+
+		</div>
+	</section>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				currentResult: 0,
+				currentInput: '',
+				currentOperator: null,
+				showingResult: false,
+			}
+		}, 
+
+		created() {
+			window.addEventListener('keyup', this.handleKeyup);
+		},
+
+		computed: {
+			currentInputAsNumber() {
+				return Number(this.currentInput)
+			},
+		},
+
+		methods: {
+			calculateResult() {
+				switch(this.currentOperator) {
+					case '+':
+						this.currentResult += this.currentInputAsNumber;
+						break;
+					case '-':
+						this.currentResult -= this.currentInputAsNumber;
+						break;
+					case '/':
+						this.currentResult /= this.currentInputAsNumber;
+						break;
+					case '*':
+						this.currentResult *= this.currentInputAsNumber;
+						break;
+					default: 
+						this.currentResult = this.currentInputAsNumber;
+				}
+			}, 
+
+			clearResult() {
+				this.currentResult = 0;
+				this.currentInput = '';
+				this.currentOperator = null;
+				this.showingResult = false;
+			},
+
+			handleOperatorInput(operator) {
+				this.calculateResult();
+				this.currentOperator = operator;
+				this.currentInput = '';
+				this.showingResult = true;
+			},
+
+			handleDigitInput(digit) {
+				this.currentInput += digit;
+				this.showingResult = false;
+			},
+			
+			handleEqualsInput() {
+				this.calculateResult();
+				this.showingResult = true;
+			},
+
+			handleKeyup(event) {
+				switch(event.key) {
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+						this.handleDigitInput(event.key)
+						break;
+					case '+':
+					case '-':
+					case '/':
+					case '*':
+						this.handleOperatorInput(event.key)
+						break;
+					case '.':
+					case ',':
+						this.handleDigitInput('.')
+						break;
+					case '=':
+					case 'Enter':
+						this.handleEqualsInput()
+						break;
+					case 'Escape':
+					case 'Backspace':
+						this.clearResult()
+						break;
+				}
+			}
+		}
+	}
+</script>
+
+	
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Inter&display=swap');
+
+	.calculator-main {
+		height: 100%;
+		width: 50%;
+		background: #d2a24c;
+		padding: 0.75rem;
+	}
+
+	.calculator-main__headline {
+		font-family: 'Bungee Shade', cursive;
+		color: #281b10;
+		margin-top: 1rem;
+		font-size: 1.1rem;
+		text-align: center;
+	}
+
+	.calculator-main__output {
+		display: flex;
+		align-items: center;
+		justify-content: right;
+		width: 80%;
+		height: 2rem;
+		padding: 1rem;
+		margin: 2rem 2rem 1rem 1.5rem;
+		border: 0.2rem solid #6f5643;
+		background: #fff;
+		border-radius: 0.5rem;
+
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+
+	.calculator-main__digits {
+		width: 100%;
+		height: 55%;
+		display: grid;
+		grid-template-columns: repeat(4, 25%);	
+	}
+
+	.calculator-main__button {
+		width: 90%;
+		height: 90%;
+		padding: 0.1rem;
+		margin: 0.2rem;
+		font-size: 1rem;
+		font-weight: bold;
+		cursor: pointer;
+		transition: 0.2s;
+		border-radius: 0.25rem;
+		background-color: #73bda8;
+		border: 0.2rem solid #376659;
+	}
+
+	.calculator-main__zero {
+		width: 95%;
+		grid-column: span 2;
+	}
+
+	.calculator-main__clear {
+		width: 95%;
+		grid-column: span 3;
+	}
+
+	.calculator-main__button:hover {
+		opacity: 60%;
+	}
+
+	.calculator-main__operator, .calculator-main__clear {
+		background: #eca58b;
+		border: 0.2rem solid rgb(242, 90, 90);
+	}
+</style>
